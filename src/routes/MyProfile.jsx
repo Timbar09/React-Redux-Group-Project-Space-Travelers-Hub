5e76641d82de8a55daf27e60d06962c098e1f752
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-03-08 12:53:40 +0200

    Display joined missions in MyProfile.jsx

--- src/routes/MyProfile.jsx
+++ src/routes/MyProfile.jsx
@@ -1,7 +1,36 @@
+import { useSelector } from 'react-redux';
+
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import styles from './MyProfile.module.css';
 
 function MyProfile() {
-  return <Container>MyProfile</Container>;
+  const { missions } = useSelector((state) => state.missions);
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h2 className="mb-4">My Missions</h2>
+          <ul className="border rounded">
+            {missions
+              .filter((mission) => mission.reserved === true)
+              .map((mission) => (
+                <li className={`${styles.missionItem} p-4`} key={mission.id}>
+                  {mission.name}
+                </li>
+              ))}
+          </ul>
+        </Col>
+        <Col>
+          <h2 className="mb-4">My Rockets</h2>
+          <ul className="border rounded">A list of rockets</ul>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default MyProfile;
