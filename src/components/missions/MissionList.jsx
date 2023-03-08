d04ffa178a02a18f09a79b173e70db1e36dd8178
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-03-08 20:45:53 +0200

    Restructure missions

--- src/components/missions/MissionList.jsx
+++ src/components/missions/MissionList.jsx
@@ -1,21 +1,27 @@
 import { useSelector } from 'react-redux';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import MissionItem from './MissionItem';
+import styles from './MissionList.module.css';
 
 function MissionList() {
   const { missions } = useSelector((state) => state.missions);
 
   return (
-    <table className="table bg-white rounded">
-      <thead>
-        <tr>
-          <th className="head">Mission</th>
-          <th className="head">Description</th>
-          <th className="head">Status</th>
-          <th className="head"> </th>
-        </tr>
-      </thead>
-      <tbody>
+    <div className="py-2 px-3 bg-white rounded">
+      <h1 className="pb-3 d-md-none">Missions</h1>
+      <Row className={`${styles.heads} pb-2 d-none d-md-flex`}>
+        <Col md={2}>Mission</Col>
+        <Col md={5} lg={6}>
+          Descriotion
+        </Col>
+        <Col md={3} lg={2}>
+          Status
+        </Col>
+        <Col md={2} />
+      </Row>
+      <ul>
         {missions.map((mission) => (
           <MissionItem
             key={mission.id}
@@ -25,8 +31,8 @@ function MissionList() {
             reserved={mission.reserved}
           />
         ))}
-      </tbody>
-    </table>
+      </ul>
+    </div>
   );
 }
 
