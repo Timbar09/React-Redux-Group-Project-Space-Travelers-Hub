22d50e945111c326c0a790129d21c33bd89874ac
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-03-11 17:35:35 +0200

    Add empty list placeholder

--- src/routes/MyProfile.jsx
+++ src/routes/MyProfile.jsx
@@ -10,31 +10,44 @@ function MyProfile() {
   const { missions } = useSelector((state) => state.missions);
   const { rocketList } = useSelector((store) => store.Rockets);
 
+  const joinedMissions = missions.filter((mission) => mission.reserved === true);
+  const reservedRockets = rocketList.filter((rocket) => rocket.isReserved === true);
+
   return (
     <Container>
       <Row>
         <Col sm={12} lg={6} className="mb-4">
           <h2 className="mb-4">My Missions</h2>
+          {/* {!joinedMissions && <div>No missions booked yet!</div>} */}
+
           <ul className="border rounded">
-            {missions
-              .filter((mission) => mission.reserved === true)
-              .map((mission) => (
+            {joinedMissions.length > 0 ? (
+              joinedMissions.map((mission) => (
                 <li className={`${styles.missionItem} p-4`} key={mission.id}>
                   {mission.name}
                 </li>
-              ))}
+              ))
+            ) : (
+              <div className={styles.emptyListMessage}>
+                <p>No missions booked yet!</p>
+              </div>
+            )}
           </ul>
         </Col>
         <Col sm={12} lg={6}>
           <h2 className="mb-4">My Rockets</h2>
           <ul className="border rounded">
-            {rocketList
-              .filter((rocket) => rocket.isReserved === true)
-              .map((rocket) => (
+            {reservedRockets.length > 0 ? (
+              reservedRockets.map((rocket) => (
                 <li className={`${styles.missionItem} p-4`} key={rocket.id}>
                   {rocket.name}
                 </li>
-              ))}
+              ))
+            ) : (
+              <div className={styles.emptyListMessage}>
+                <p>No rockets reserved yet!</p>
+              </div>
+            )}
           </ul>
         </Col>
       </Row>
