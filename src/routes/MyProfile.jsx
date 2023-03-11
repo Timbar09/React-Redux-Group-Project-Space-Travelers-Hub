49a928e04d16957bd57ea72ec6b5f4d8e3b1b511
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-03-11 18:00:48 +0200

    Add mission and rocket cancelation button

--- src/routes/MyProfile.jsx
+++ src/routes/MyProfile.jsx
@@ -1,14 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
+import { joinLeaveToggle } from '../redux/missions/missionsSlice';
+import { remReservation } from '../redux/Rockets/rocketSlice';
 import styles from './MyProfile.module.css';
 
 function MyProfile() {
   const { missions } = useSelector((state) => state.missions);
   const { rocketList } = useSelector((store) => store.Rockets);
+  const dispatch = useDispatch();
 
   const joinedMissions = missions.filter((mission) => mission.reserved === true);
   const reservedRockets = rocketList.filter((rocket) => rocket.isReserved === true);
@@ -18,13 +22,24 @@ function MyProfile() {
       <Row>
         <Col sm={12} lg={6} className="mb-4">
           <h2 className="mb-4">My Missions</h2>
-          {/* {!joinedMissions && <div>No missions booked yet!</div>} */}
 
           <ul className="border rounded">
             {joinedMissions.length > 0 ? (
               joinedMissions.map((mission) => (
                 <li className={`${styles.missionItem} p-4`} key={mission.id}>
-                  {mission.name}
+                  <Row>
+                    <Col>
+                      <p>{mission.name}</p>
+                    </Col>
+                    <Col className={styles.cancelBtn}>
+                      <Button
+                        variant="outline-danger"
+                        onClick={() => dispatch(joinLeaveToggle(mission.id))}
+                      >
+                        Leave Mission
+                      </Button>
+                    </Col>
+                  </Row>
                 </li>
               ))
             ) : (
@@ -40,7 +55,19 @@ function MyProfile() {
             {reservedRockets.length > 0 ? (
               reservedRockets.map((rocket) => (
                 <li className={`${styles.missionItem} p-4`} key={rocket.id}>
-                  {rocket.name}
+                  <Row>
+                    <Col>
+                      <p>{rocket.name}</p>
+                    </Col>
+                    <Col className={styles.cancelBtn}>
+                      <Button
+                        variant="outline-danger"
+                        onClick={() => dispatch(remReservation(rocket.id))}
+                      >
+                        Cancel Reservation
+                      </Button>
+                    </Col>
+                  </Row>
                 </li>
               ))
             ) : (
