d06698561a04968e8eb853abbd23959a10a2dedf
Miles Mosweu <tmtmosweu@gmail.com>
2024-08-18 00:41:40 +0200

    Add RocketImageCarousel component for displaying a carousel of rocket images

---
+++ src/components/Rocket/RocketImageCarousel.jsx
@@ -0,0 +1,83 @@
+import PropTypes from 'prop-types';
+
+import styles from './index.module.css';
+
+function Button({ nextOrPrev, cardId }) {
+  return (
+    <button
+      className={`carousel-control-${nextOrPrev}`}
+      type="button"
+      data-bs-target={`#${cardId}`}
+      data-bs-slide={nextOrPrev}
+    >
+      <span
+        className={`carousel-control-${nextOrPrev}-icon`}
+        aria-hidden="true"
+      />
+      <span className="visually-hidden">{nextOrPrev}</span>
+    </button>
+  );
+}
+
+function CarouselIndicators({ imageUrls, cardId }) {
+  return (
+    <div className={`${styles.carouselIndicators} carousel-indicators`}>
+      {imageUrls.map((_, index) => (
+        <button
+          type="button"
+          data-bs-target={`#${cardId}`}
+          data-bs-slide-to={index}
+          className={index === 0 ? 'active' : ''}
+          aria-label={`Slide ${index}`}
+          key={cardId}
+        />
+      ))}
+    </div>
+  );
+}
+
+function RocketImageCarousel({ imageUrls, cardId }) {
+  return (
+    <div id={cardId} className={`${styles.carousel} carousel slide rounded-2`}>
+      <CarouselIndicators imageUrls={imageUrls} cardId={cardId} />
+
+      <div className={`${styles.carouselInner} carousel-inner`}>
+        {imageUrls.map((url, index) => (
+          <div
+            className={`${styles.carouselItem} carousel-item ${
+              index === 0 ? 'active' : ''
+            }`}
+            key={url}
+          >
+            <img
+              src={url}
+              className={`${styles.carouselImage} d-block w-100`}
+              alt="..."
+            />
+          </div>
+        ))}
+      </div>
+
+      <Button nextOrPrev="prev" cardId={cardId} />
+
+      <Button nextOrPrev="next" cardId={cardId} />
+    </div>
+  );
+}
+
+CarouselIndicators.propTypes = {
+  imageUrls: PropTypes.arrayOf(PropTypes.string).isRequired,
+  cardId: PropTypes.string.isRequired,
+};
+
+Button.propTypes = {
+  nextOrPrev: PropTypes.string.isRequired,
+  cardId: PropTypes.string.isRequired,
+};
+
+RocketImageCarousel.propTypes = {
+  imageUrls: PropTypes.arrayOf(PropTypes.string).isRequired,
+  cardId: PropTypes.string.isRequired,
+};
+
+export default RocketImageCarousel;
