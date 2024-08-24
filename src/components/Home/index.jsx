6edad229be84fa74a2c15b1acf95186b109bd6ee
Miles Mosweu <tmtmosweu@gmail.com>
2024-08-24 22:04:18 +0200

    Update Home component with title and description

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -5,8 +5,17 @@ import styles from './index.module.css';
 function Home() {
   return (
     <div className={`${styles.homePage} py-4`}>
-      <Container className="d-flex flex-column align-items-center justify-content-center">
-        <h1>Home</h1>
+      <Container
+        className={`${styles.container} h-100 d-flex flex-column gap-4 align-items-center justify-content-center position-relative`}
+      >
+        <h1 className={`${styles.title} text-center position-relative`}>
+          <span>SPACE</span>
+          <span> TRAVELERS&apos; HUB </span>
+        </h1>
+
+        <p className={`${styles.description} text-center position-relative`}>
+          A place where space enthusiasts explore the universe together.
+        </p>
       </Container>
     </div>
   );
