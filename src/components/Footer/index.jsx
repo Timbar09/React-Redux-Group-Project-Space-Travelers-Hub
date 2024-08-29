b5ff6ef046f8bbff840d5937ed024d1406379060
Miles Mosweu <tmtmosweu@gmail.com>
2024-08-29 16:26:23 +0200

    Render CustomLink component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,26 +2,27 @@ import { useLocation } from 'react-router-dom';
 import dayjs from 'dayjs';
 import Container from 'react-bootstrap/Container';
 
+import CustomLink from '../CustomLink';
+
 import styles from './index.module.css';
 
 function Footer() {
   const location = useLocation();
+  const isHome = location.pathname === '/';
   const currentYear = dayjs().format('YYYY');
   const copyrightContent = `${currentYear} - All Rights Reserved - `;
 
   return (
     <footer
       className={`${styles.footer} text-center ${
-        location.pathname === '/' ? styles.homeFooter : ''
+        isHome ? styles.homeFooter : ''
       }`}
     >
       <Container className="py-4 d-flex flex-wrap justify-content-center align-items-center gap-3 justify-content-md-between">
         <p className="m-0">
           &copy;
           {copyrightContent}
-          <a href="#" className="" target="_blank" rel="noreferrer">
-            Space Traveler&apos;s Hub
-          </a>
+          <CustomLink to="#" text="Space Traveler's Hub" target="_blank" />
         </p>
       </Container>
     </footer>
