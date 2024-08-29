74824c9caeda167695caac53cd9d0cd5840d1ff6
Miles Mosweu <tmtmosweu@gmail.com>
2024-08-29 16:09:58 +0200

    Add auto updatable copyright year to Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,13 @@
 import { useLocation } from 'react-router-dom';
+import dayjs from 'dayjs';
 import Container from 'react-bootstrap/Container';
 
 import styles from './index.module.css';
 
 function Footer() {
   const location = useLocation();
+  const currentYear = dayjs().format('YYYY');
+  const copyrightContent = `${currentYear} - All Rights Reserved - `;
 
   return (
     <footer
@@ -12,9 +15,10 @@ function Footer() {
         location.pathname === '/' ? styles.homeFooter : ''
       }`}
     >
-      <Container className="py-4 d-flex flex-wrap justify-content-between align-items-center">
+      <Container className="py-4 d-flex flex-wrap justify-content-center align-items-center gap-3 justify-content-md-between">
         <p className="m-0">
-          &copy; 2021 - All Rights Reserved -{' '}
+          &copy;
+          {copyrightContent}
           <a href="#" className="" target="_blank" rel="noreferrer">
             Space Traveler&apos;s Hub
           </a>
