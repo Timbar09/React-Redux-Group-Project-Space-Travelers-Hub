7be03ce8b662a0ad396ec5ac36fcae7c6c713552
Miles Mosweu <tmtmosweu@gmail.com>
2024-09-10 21:05:25 +0200

    Update rocketReducer import path

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './missions/missionSlice';
-import rocketReducer from './rockets/rocketSlice';
+import rocketReducer from './rockets-temp/rocketSlice';
 
 const store = configureStore({
   reducer: {
